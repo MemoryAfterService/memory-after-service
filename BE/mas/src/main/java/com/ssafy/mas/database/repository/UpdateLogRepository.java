499ed350bb74f813e5e09f85cc1fb081fc6e7653
강민성 <pfcskms1997@naver.com>
2022-10-03 11:38:31 +0900

    [BE] Feat: (407) FileUpload Controller 보완 - 강민성

--- BE/mas/src/main/java/com/ssafy/mas/database/repository/UpdateLogRepository.java
+++ BE/mas/src/main/java/com/ssafy/mas/database/repository/UpdateLogRepository.java
@@ -1,9 +1,23 @@
 package com.ssafy.mas.database.repository;
 
+import com.ssafy.mas.database.entity.Member;
 import com.ssafy.mas.database.entity.UpdateLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 
 @Repository
 public interface UpdateLogRepository extends JpaRepository<UpdateLog, Long> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE UpdateLog as l " +
+            "SET l.count = l.count + 1, l.lastUploadDate = :date " +
+            "WHERE l.member = :member")
+    public int updateProfileUploadLog(@Param("member") Member member,
+                                      @Param("date") LocalDate date);
 }
